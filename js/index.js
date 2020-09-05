c3cdab65e97a741e05ac71d62b6ff1639cbb0882
fuxinjian <893269251@qq.com>
2020-09-05 18:01:26 +0800

    面试

---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function () {
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: true,
+  });
+})
